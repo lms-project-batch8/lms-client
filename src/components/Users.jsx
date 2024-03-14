b62e600517fd8589bc2627c5d82ea63482290b33
Subhadeeep25 <subhadeep25aeccse@gmail.com>
2024-03-14 15:12:46 +0530

    superuser edit delete button

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -8,108 +8,103 @@ import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import axios from "axios";
-import EditIcon from "@mui/icons-material/Edit";
-
+import EditRoundedIcon from "@mui/icons-material/EditRounded";
+import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded";
 const columns = [
-    { id: "user_id", label: "user_id", minWidth: 180 },
-    { id: "user_name", label: "user_name", minWidth: 250 },
-    { id: "user_email", label: "user_email", minWidth: 300 },
-    { id: "user_password", label: "user_password", minWidth: 250 },
-    { id: "user_role", label: "user_role", minWidth: 180 },
+  { id: "user_id", label: "user_id", minWidth: 130 },
+  { id: "user_name", label: "user_name", minWidth: 200 },
+  { id: "user_email", label: "user_email", minWidth: 250 },
+  { id: "user_password", label: "user_password", minWidth: 200 },
+  { id: "user_role", label: "user_role", minWidth: 180 },
 ];
 
 export default function Users() {
-    const [users, setUsers] = useState([]);
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
-    useEffect(() => {
-        const getUsers = async () => {
-            const res = await axios.get(
-                "https://lms-server-tktv.onrender.com/users"
-            );
-            setUsers(res.data);
-        };
-
-        getUsers();
-    }, []);
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
+  useEffect(() => {
+    const getUsers = async () => {
+      const res = await axios.get("https://lms-server-tktv.onrender.com/users");
+      setUsers(res.data);
     };
 
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(event.target.value);
-        setPage(0);
-    };
+    getUsers();
+  }, []);
 
-    return (
-        <Paper
-            sx={{
-                width: "100%",
-                height: "100vh", // Ensure Paper takes full viewport height
-                display: "flex",
-                flexDirection: "column", // Stack children vertically
-                justifyContent: "space-between", // Spread out the content
-                overflow: "hidden",
-                padding: "20px",
-                marginBottom: "20px",
-            }}
-        >
-            <TableContainer sx={{ flexGrow: 1, overflow: "auto" }}>
-                {/* Make TableContainer flexible and scrollable */}
-                <Table stickyHeader aria-label="sticky table">
-                    <TableHead sx={{ background: "blueviolet" }}>
-                        <TableRow>
-                            {columns.map((column) => (
-                                <TableCell
-                                    key={column.id}
-                                    align={column.align}
-                                    style={{ minWidth: column.minWidth }}
-                                >
-                                    {column.label}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {users
-                            .slice(
-                                page * rowsPerPage,
-                                page * rowsPerPage + rowsPerPage
-                            )
-                            .map((user, index) => (
-                                <TableRow
-                                    hover
-                                    role="checkbox"
-                                    tabIndex={-1}
-                                    key={index}
-                                >
-                                    {columns.map((column) => {
-                                        const value = user[column.id];
-                                        return (
-                                            <TableCell
-                                                key={column.id}
-                                                align={column.align}
-                                            >
-                                                {value}
-                                            </TableCell>
-                                        );
-                                    })}
-                                </TableRow>
-                            ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[10, 25, 100]}
-                component="div"
-                count={users.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-        </Paper>
-    );
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper
+      sx={{
+        width: "100%",
+        height: "100vh", // Ensure Paper takes full viewport height
+        display: "flex",
+        flexDirection: "column", // Stack children vertically
+        justifyContent: "space-between", // Spread out the content
+        overflow: "hidden",
+        padding: "20px",
+        marginBottom: "20px",
+      }}
+    >
+      <TableContainer sx={{ flexGrow: 1, overflow: "auto" }}>
+        {/* Make TableContainer flexible and scrollable */}
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead sx={{ background: "blueviolet" }}>
+            <TableRow>
+              {columns.map((column) => (
+                <>
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+                </>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((user, index) => (
+                <TableRow hover role="checkbox" tabIndex={-1} key={index}>
+                  {columns.map((column) => {
+                    const value = user[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {value}
+                      </TableCell>
+                    );
+                  })}
+                  <TableCell align="center">
+                    <div className="flex justify-center items-center gap-4">
+                      <EditRoundedIcon color="action" onClick={()=>{}} sx={{cursor:"pointer"}}/>
+                      <DeleteForeverRoundedIcon color="warning" onClick={()=>{}} sx={{cursor:"pointer"}}/>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={users.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
 }
