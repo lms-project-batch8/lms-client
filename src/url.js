cb78810b4054c69097e21701fe44e22724a5f907
lms-project-batch8 <lms.project.batch8@gmail.com>
2024-05-05 00:48:37 +0530

    a

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-export const backend = `https://lms-server-15hc.onrender.com`;
-export const frontend = `https://pursuit-lms.netlify.app`;
+export const backend = `https://lms-server-15hc.onrender.com`
+export const frontend = `https://pursuit-lms.netlify.app`
