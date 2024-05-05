a15e1112651595fd841ed404e454454cc07a5ec8
lms-project-batch8 <lms.project.batch8@gmail.com>
2024-05-05 15:38:13 +0530

    backend url changed to vercel

--- src/url.js
+++ src/url.js
@@ -1,2 +1,3 @@
-export const backend = `https://lms-server-15hc.onrender.com`
+// export const backend = `https://lms-server-15hc.onrender.com`
+export const backend = `https://pursuit-lms-server.vercel.app`
 export const frontend = `https://pursuit-lms.netlify.app`
