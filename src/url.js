5a788686dc00a1cbfc9b8c25407e463a8998b753
lms-project-batch8 <lms.project.batch8@gmail.com>
2024-05-06 00:31:27 +0530

    bugs

--- src/url.js
+++ src/url.js
@@ -1,4 +1,4 @@
 // export const backend = `https://lms-server-15hc.onrender.com`
-// export const backend = `https://pursuit-lms-server.vercel.app`
-export const backend = `http://localhost:20190`;
+export const backend = `https://pursuit-lms-server.vercel.app`;
+// export const backend = `http://localhost:20190`;
 export const frontend = `https://pursuit-lms.netlify.app`;
